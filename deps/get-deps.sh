04b2699594d50111344b636dfd90124d1f12212f
Christoph Finkensiep <chfin@chfin.de>
2025-10-21 16:05:10 +0200

    fix libtorch filenames

--- deps/get-deps.sh
+++ deps/get-deps.sh
@@ -8,11 +8,11 @@ CUDA_VERSION="130"
 
 case $1 in
     cpu)
-        filename="libtorch-cxx11-abi-shared-with-deps-${VERSION}+cpu.zip"
+        filename="libtorch-shared-with-deps-${VERSION}+cpu.zip"
         url="https://download.pytorch.org/libtorch/cpu/libtorch-shared-with-deps-${VERSION}%2Bcpu.zip"
         ;;
     cuda)
-        filename="libtorch-cxx11-abi-shared-with-deps-${VERSION}+cuda.zip"
+        filename="libtorch-shared-with-deps-${VERSION}+cuda.zip"
         url="https://download.pytorch.org/libtorch/cu${CUDA_VERSION}/libtorch-shared-with-deps-${VERSION}%2Bcu${CUDA_VERSION}.zip"
         ;;
     rocm)
