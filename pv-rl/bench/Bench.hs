9ef6490f66241595c8037c591ed4ecf91917c32c
Christoph Finkensiep <chfin@chfin.de>
2025-11-17 11:52:40 +0100

    add benchmarks for forward pass

---
+++ pv-rl/bench/Bench.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DataKinds #-}
+
+module Main where
+
+import Common
+import GreedyParser
+import PVGrammar
+import RL qualified
+
+import Criterion.Main
+import Data.List.NonEmpty qualified as NE
+import Torch.Typed qualified as TT
+
+{- | Benchmark the forward pass with varying numbers of actions.
+Batches the actions before benchmarking.
+-}
+benchForward :: (RL.IsValidDevice dev) => String -> RL.QModel dev -> RL.PVState -> RL.PVAction -> Benchmark
+benchForward what model state action =
+  bgroup
+    ("forward " <> what)
+    [ benchN 1
+    , benchN 10
+    , benchN 100
+    ]
+ where
+  benchN n = RL.withBatchedEncoding state (action NE.:| replicate (n - 1) action) $ \enc ->
+    bench (show n <> " actions") $ nf (RL.runBatchedPolicy model) enc
+
+main :: IO ()
+main = do
+  modelCPU <- RL.mkQModel @'(TT.CPU, 0)
+  modelGPU <- RL.mkQModel @'(TT.CUDA, 0)
+  defaultMain
+    [ benchForward "CPU" modelCPU state action
+    , benchForward "GPU" modelGPU state action
+    ]
+ where
+  -- TODO: better states and actions
+  state :: RL.PVState
+  state =
+    GSSemiOpen
+      (Path Nothing (Notes mempty) $ PathEnd Nothing)
+      (Notes mempty)
+      (Path mempty (Notes mempty) $ PathEnd mempty)
+      []
+  action :: RL.PVAction
+  action = Left $ ActionSingle (SingleParent Start mempty Stop) (LMSingleFreeze $ FreezeOp mempty)
